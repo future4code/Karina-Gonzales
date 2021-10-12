deccd15035c3c8913e96354f3da65ecdac721f96
karinakatzllberger <karinakatzllberger@gmail.com.br>
2021-10-12 12:51:37 -0300

    organizacao de pastas

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const nomeDoUsuario = prompt("qual é seu sobrenome?")
 
 const idade = prompt("qual é sua idade?")
 
-console.log("Olá, "mwu nome é , nomeDoUsuario, "eu tenho", idade, "anos")
+console.log("Olá, "meu nome é , nomeDoUsuario, "eu tenho", idade, "anos")
 
