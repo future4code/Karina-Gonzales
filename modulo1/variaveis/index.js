1e8dabb30cc3cdb27a744409599ab508816e1db2
karinakatzllberger <karinakatzllberger@gmail.com.br>
2021-10-12 11:24:20 -0300

    Aula de variaveis 04.10

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -9,5 +9,13 @@ const nomeDoUsuario = prompt("qual é seu sobrenome?")
 
 const idade = prompt("qual é sua idade?")
 
+<<<<<<< Updated upstream:modulo1/variaveis/index.js
 console.log("Olá, "mwu nome é , nomeDoUsuario, "eu tenho", idade, "anos")
+=======
+<<<<<<< HEAD
+console.log("Olá, "meu nome é" , nomeDoUsuario, "eu tenho", idade, "anos")
+=======
+console.log("Olá, "mwu nome é , nomeDoUsuario, "eu tenho", idade, "anos")
+>>>>>>> e7fd3a0643df7b3d741aabf204270dd5b3517e15
+>>>>>>> Stashed changes:index.js
 
