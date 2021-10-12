1627d8eecc2cdefaefd6f4859e97e22510c4ef83
karinakatzllberger <karinakatzllberger@gmail.com.br>
2021-10-12 11:32:51 -0300

    Aula de variaveis

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,21 +1,88 @@
-//imprime a mensagem no consoledo navegador
 
-console.log ("Olá mundo!")
 
-//pegarinformações do usuario
 
-let nome = prompt("Qual éseu nome?")
-const nomeDoUsuario = prompt("qual é seu sobrenome?")
+//Exercicio 01
 
-const idade = prompt("qual é sua idade?")
 
-<<<<<<< Updated upstream:modulo1/variaveis/index.js
-console.log("Olá, "mwu nome é , nomeDoUsuario, "eu tenho", idade, "anos")
-=======
-<<<<<<< HEAD
-console.log("Olá, "meu nome é" , nomeDoUsuario, "eu tenho", idade, "anos")
-=======
-console.log("Olá, "mwu nome é , nomeDoUsuario, "eu tenho", idade, "anos")
->>>>>>> e7fd3a0643df7b3d741aabf204270dd5b3517e15
->>>>>>> Stashed changes:index.js
+let a = 10
+let b = 10
 
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+// o resultado será:
+// 10
+// 10, 5 
+
+______________________________________________
+
+
+let a = 10
+let b = 20
+let c = a
+c = a 
+b = c 
+a = b 
+
+console.log(a, b, c)
+// resultado é: 10 10 10 (obs. variavel let C nao estava declarada, declarei e resolveu).
+
+______________________________________________
+
+
+
+let horasDia = prompt("Quantas horas você trabalha por dia?")
+let valorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasDia/valorDia} por hora`)
+
+
+______________________________________________
+
+
+const meuNome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade","")
+
+console.log(meuNome)
+console.log(idade)
+
+console.log(typeof meuNome)
+console.log(typeof (idade))
+
+alert(`Olá ${meuNome}, você tem ${idade} anos.`)
+
+// typeof declarou ambas como string, porque esta tendo interação com o usuario, pois é um prompt.
+
+
+______________________________________________
+// Exercicio 02
+
+const duvida1 = "Você esta usando uma roupa azul hoje?"
+const duvida2 = "Você usa óculos?"
+const duvida3 = "Você é careca?"
+
+const resposta1 = prompt(duvida1);
+const resposta2 = prompt(duvida2);
+const resposta3 = prompt(duvida3);
+
+console.log(duvida1, "-", resposta1);
+console.log(duvida2, "-", resposta2);
+console.log(duvida3, "-", resposta3);
+
+
+
+______________________________________________
+// Exercicio 03
+
+
+let a = 10
+let b = 25
+
+// Aqui faremos uma lógica para trocar os valores
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", b) // O novo valor de a é 25
+console.log("O novo valor de b é", a) // O novo valor de b é 10
+
+//fim.
